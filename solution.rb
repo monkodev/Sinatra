2507bace415525428a6e1f8ab76bc020d59533db
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-02 20:30:21 -0300

    MOdifica solution.rb saludame 2

--- solution.rb
+++ solution.rb
@@ -7,6 +7,16 @@ get '/' do
 		<<-HTML
 			<h1>Hola #{params[:nombre]}!</h1>	
 		HTML
-				
+
 	end  
+end
+
+get '/makers/:nombre' do
+	
+		<<-HTML
+			<h1>Hola #{params[:nombre].capitalize!}!</h1>	
+		HTML
+
+	
+
 end
