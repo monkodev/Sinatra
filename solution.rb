710fcce49fe3f4ad9c94a1a0ea9c459db2b54a6c
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-03 09:13:30 -0300

    Agregar reto 3 usando post

--- solution.rb
+++ solution.rb
@@ -1,14 +1,7 @@
 require 'sinatra'
 
 get '/' do
-	unless params[:nombre]
-		"Hola desconocido!"
-	else
-		<<-HTML
-			<h1>Hola #{params[:nombre]}!</h1>	
-		HTML
-
-	end  
+	erb :index
 end
 
 get '/makers/:nombre' do
@@ -16,7 +9,8 @@ get '/makers/:nombre' do
 		<<-HTML
 			<h1>Hola #{params[:nombre].capitalize!}!</h1>	
 		HTML
+end
 
-	
-
+post '/obj' do
+	"Hola! #{params[:verbo]}"
 end
